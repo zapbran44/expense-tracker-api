ce8d887bb5f4f5e6c7b942c944b243a1f2a15f65
zapbran44 <zapbran44@hotmail.com>
2025-02-07 11:14:02 +0000

    added model for expenses and tests

--- restapi/models.py
+++ restapi/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Expense(models.Model):
+    amount = models.FloatField()
+    merchant = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, blank=True, null=True)
+    category = models.CharField(max_length=255)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
--- restapi/tests.py
+++ restapi/tests.py
@@ -1,11 +1,17 @@
-# from django.test import TestCase
-from unittest import TestCase
+from django.test import TestCase
+from restapi import models
 
 # Create your tests here.
-def two_integers_sum(a, b):
-    return a + b
-
-
-class TestSum(TestCase):
-    def test_sum(self):
-        self.assertEqual(two_integers_sum(1, 2), 3)
+class TestModels(TestCase):
+    def test_expense(self):
+        expense = models.Expense.objects.create(
+            amount=249.99,
+            merchant="amazon",
+            description="anc headphones",
+            category="music",
+        )
+        inserted_expense = models.Expense.objects.get(pk=expense.id)
+        self.assertEqual(249.99, inserted_expense.amount)
+        self.assertEqual("amazon", inserted_expense.merchant)
+        self.assertEqual("anc headphones", inserted_expense.description)
+        self.assertEqual("music", inserted_expense.category)
